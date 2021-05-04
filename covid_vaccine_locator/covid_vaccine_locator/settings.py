ad62073c1e54edbd82e5979418a197f64d023fc1
7wick <saatwick.chandra@acquia.com>
2021-05-05 01:54:31 +0530

    Added app and deleted db

--- covid_vaccine_locator/covid_vaccine_locator/settings.py
+++ covid_vaccine_locator/covid_vaccine_locator/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # add your apps
+    'rest_framework',
+    'locator',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +76,14 @@ WSGI_APPLICATION = 'covid_vaccine_locator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES not required for this project. Create db.sqlite3 at root, if required
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
